801529c9d6031a5952ad2af0805ba7b21eadf890
Mahmoud412 <52178976+Mahmoud412@users.noreply.github.com>
2020-05-15 21:19:18 +0700

    Adding routes and links to the nav bar

--- DataingApp-SPA/src/app/nav/nav.component.ts
+++ DataingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,7 @@ export class NavComponent implements OnInit {
 
   model: any = {}; // saving out username and password
 
-  constructor(public authService: AuthService , private alertify: AlertifyService ) { }
+  constructor(public authService: AuthService , private alertify: AlertifyService , private router: Router) { }
 
   ngOnInit() {
   }
@@ -23,6 +24,8 @@ export class NavComponent implements OnInit {
 
     }, error  =>  {
       this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -35,5 +38,6 @@ export class NavComponent implements OnInit {
   logout(){
     localStorage.removeItem('token');  // remove the token from the localstorage
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 }
