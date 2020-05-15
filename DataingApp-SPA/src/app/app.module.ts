a81646b941603532fdcee1d000c084c34ede63ec
Mahmoud412 <52178976+Mahmoud412@users.noreply.github.com>
2020-05-15 20:47:50 +0700

    Adding 3d parrty Components

--- DataingApp-SPA/src/app/app.module.ts
+++ DataingApp-SPA/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 
 
 import { AppComponent } from './app.component';
@@ -14,6 +16,7 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -26,7 +29,10 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BrowserAnimationsModule,
+      BsDropdownModule.forRoot()
+
    ],
    providers: [
 
--- DataingApp-SPA/src/app/nav/nav.component.ts
+++ DataingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,7 @@ export class NavComponent implements OnInit {
 
   model: any = {}; // saving out username and password
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService , private alertify: AlertifyService ) { }
 
   ngOnInit() {
   }
@@ -18,23 +19,21 @@ export class NavComponent implements OnInit {
   login(){
     this.authService.login(this.model).subscribe(next => {
 
-      console.log('logged in successfuly');
+      this.alertify.success('logged in successfuly');
 
     }, error  =>  {
-       console.log(error);
+      this.alertify.error(error);
     });
   }
 
   loggedIn(){
 
-    const token = localStorage.getItem('token'); // getting the token form localstorage
-
-    return !!token;  // return true or false value
+   return this.authService.loggedIn();
 
   }
 
   logout(){
     localStorage.removeItem('token');  // remove the token from the localstorage
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 }
