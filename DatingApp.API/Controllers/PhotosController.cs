93aa6b342281a8efcfc9c69b92fb0b7a624feb0c
Mahmoud412 <52178976+Mahmoud412@users.noreply.github.com>
2020-06-15 19:40:39 +0700

    Added upload Photo function

--- DatingApp.API/Controllers/PhotosController.cs
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -1,7 +1,13 @@
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using AutoMapper;
 using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 using DatingApp.API.Data;
+using DatingApp.API.Dtos;
 using DatingApp.API.Helpers;
+using DatingApp.API.Properties.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -37,6 +43,70 @@ namespace DatingApp.API.Controllers
            _cloudinary = new Cloudinary(acc);
 
         }
+        [HttpGet("{id}", Name = "GetPhoto")]
+        public async Task<IActionResult> GetPhoto (int id)
+        {
+            var photoFromRepo =  await _repo.GetPhoto(id);
 
-}
+            var photo = _mapper.Map<PhotoForReturnDto>(photoFromRepo);
+
+            return Ok(photo);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddphotoForUser(int userId , 
+           [FromForm] PhotoForCreationDto photoForCreationDto)
+        {
+             if(userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                 return Unauthorized();
+        
+            var userFromRepo = await _repo.GetUser(userId);
+            
+            // store the prop file to the file
+            var file = photoForCreationDto.File;
+
+            // store the result that we getting back form cloudinary
+            var uploadResult =  new ImageUploadResult();
+
+            if(file.Length > 0 )
+            {
+                // dispose of what the file inside memory one's we completed this method
+                using (var stream  = file.OpenReadStream())
+                {
+                    var uploadprams = new ImageUploadParams()
+                    {
+                        File = new FileDescription(file.Name , stream),
+                        Transformation = new Transformation()
+                        .Width(500).Height(500).Crop("fill").Gravity("face")
+                    };
+
+                    uploadResult = _cloudinary.Upload(uploadprams);
+
+
+                }
+            }
+
+            photoForCreationDto.Url =  uploadResult.Uri.ToString();
+            photoForCreationDto.PublicId = uploadResult.PublicId;
+
+            var photo = _mapper.Map<Photo>(photoForCreationDto);
+
+            if(!userFromRepo.Photos.Any(u => u.IsMain))
+                photo.IsMain = true;
+           
+            userFromRepo.Photos.Add(photo);
+
+        
+            if(await _repo.SaveAll())
+            {
+                 var photoToReturn = _mapper.Map<PhotoForReturnDto>(photo);
+                return CreatedAtRoute("GetPhoto" , new { userId = userId , id = photo.Id},
+                photoToReturn);
+            }
+            
+
+            return BadRequest("Could not add the photo");
+        }
+    
+    }
 }
